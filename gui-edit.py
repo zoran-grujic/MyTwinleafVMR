22a12ea6152f94351b9639a08504947e35b77264
BGMAGLAB <86105573+BGMAGLAB@users.noreply.github.com>
2022-10-04 16:50:18 +0200

    Add std

--- gui-edit.py
+++ gui-edit.py
@@ -3,7 +3,8 @@ Start Qt Designer to edit gui.ui file
 Then run gui-compile.py to convert ui file into py
 """
 
+
 import os
-path_to_qtdesigner = "C:\\Users\\Zoran\\Anaconda3\\Library\\bin\\designer.exe"
-cmd = "START " + path_to_qtdesigner + " gui.ui"
+cmd = "START C:\\Users\\bgmaglab\\Anaconda3\\Library\\bin\\designer.exe gui.ui"
 os.system(cmd)
+
